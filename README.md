19cf1baa7146db89574fb52ac3c4387662fb4621
syuilo <syuilotan@yahoo.co.jp>
2016-09-10 16:26:04 +0900

    Initial commit :cookie:

---
+++ README.md
@@ -0,0 +1,12 @@
+Misskey Text
+============
+
+A *misskey* text analyzer
+
+Installation
+------------
+`$ npm install misskey-text`
+
+License
+-------
+[MIT](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,47 @@
+function analyze(source) {
+	
+	const tokens = [];
+
+	while (source != '') {
+		// URL
+		if (/^https?:\/\/[a-zA-Z\-~_\.:%\?=&]+/.test(source)) {
+			const link = source.match(/^https?:\/\/[a-zA-Z\-~_\.:%\?=&]+/)[0];
+			tokens.push({
+				type: 'link',
+				content: link
+			});
+			source = source.substr(link.length);
+		}
+		// Mention
+		else if (/^@[a-zA-Z\-]+/.test(source)) {
+			const mention = source.match(/^@[a-zA-Z\-]+/)[0];
+			tokens.push({
+				type: 'mention',
+				content: mention.substr(1)
+			});
+			source = source.substr(mention.length);
+		} 
+		// Text
+		else {
+			const last = tokens.pop();
+			// 末尾がテキストなら連結
+			if (last == undefined || last.type != 'text') {
+				tokens.push(last);
+				tokens.push({
+					type: 'text',
+					content: source[0]
+				});
+			} else {
+				tokens.push({
+					type: 'text',
+					content: last.content + source[0]
+				});
+			}
+			source = source.substr(1);
+		}
+	}
+
+	return tokens;
+}
+
+module.exports = analyze;

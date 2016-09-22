5ed40c970bd4a1fefc15ae2b5289ef9f88195f7c
syuilo <syuilotan@yahoo.co.jp>
2016-09-22 17:26:41 +0900

    Refactoring, Bug fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
+var urlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+var mentionRegExp = /@[a-zA-Z\-]+/;
+var hashtagRegExp = /\s#[^\s]+/;
+
 function analyze(source) {
 	
 	var tokens = [];
 
+	function addText(text) {
+		var last = tokens.pop();
+		// 末尾がテキストなら連結
+		if (last == undefined) {
+			tokens.push({
+				type: 'text',
+				content: text
+			});
+		} else if (last.type != 'text') {
+			tokens.push(last);
+			tokens.push({
+				type: 'text',
+				content: text
+			});
+		} else {
+			tokens.push({
+				type: 'text',
+				content: last.content + text
+			});
+		}
+	}
+
 	while (source != '') {
 		// URL
-		if (/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(source)) {
-			var link = source.match(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/)[0];
+		if (new RegExp('^' + urlRegExp.source).test(source)) {
+			var link = source.match(new RegExp('^' + urlRegExp.source))[0];
 			tokens.push({
 				type: 'link',
 				content: link
@@ -13,8 +39,8 @@ function analyze(source) {
 			source = source.substr(link.length);
 		}
 		// Mention
-		else if (/^@[a-zA-Z\-]+/.test(source)) {
-			var mention = source.match(/^@[a-zA-Z\-]+/)[0];
+		else if (new RegExp('^' + mentionRegExp.source).test(source)) {
+			var mention = source.match(new RegExp('^' + mentionRegExp.source))[0];
 			tokens.push({
 				type: 'mention',
 				content: mention.substr(1)
@@ -22,35 +48,18 @@ function analyze(source) {
 			source = source.substr(mention.length);
 		}
 		// Hashtag
-		else if (/^#[^\s]+/.test(source)) {
-			var hashtag = source.match(/^#[^\s]+/)[0];
+		else if (new RegExp('^' + hashtagRegExp.source).test(source)) {
+			var hashtag = source.match(new RegExp('^' + hashtagRegExp.source))[0];
+			addText(' ');
 			tokens.push({
 				type: 'hashtag',
-				content: hashtag.substr(1)
+				content: hashtag.substr(2)
 			});
 			source = source.substr(hashtag.length);
 		}
 		// Text
 		else {
-			var last = tokens.pop();
-			// 末尾がテキストなら連結
-			if (last == undefined) {
-				tokens.push({
-					type: 'text',
-					content: source[0]
-				});
-			} else if (last.type != 'text') {
-				tokens.push(last);
-				tokens.push({
-					type: 'text',
-					content: source[0]
-				});
-			} else {
-				tokens.push({
-					type: 'text',
-					content: last.content + source[0]
-				});
-			}
+			addText(source[0]);
 			source = source.substr(1);
 		}
 	}
